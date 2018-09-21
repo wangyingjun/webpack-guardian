d3582a454da112a018ce5a8f2a60958014adbb08
王英俊 <wangyingjun@mistong.com>
2018-09-21 14:34:50 +0800

    1.0.6

--- src/build.js
+++ src/build.js
@@ -4,8 +4,24 @@ const webpackConfig = require('./getConfig/build');
 module.exports = () => {
     rimraf.sync(webpackConfig.output.path);
     Webpack(webpackConfig,  (err, stats) => {
-        if(err){
-            console(err)
+        if (err) {
+            console.error(err.stack || err);
+            if (err.details) {
+                console.error(err.details);
+            }
+            return;
+        }
+    
+        const info = stats.toJson({
+            colors: true,
+        });
+    
+        if (stats.hasErrors()) {
+            console.error(info.errors);
+        }
+    
+        if (stats.hasWarnings()) {
+            console.warn(info.warnings);
         }
     })
 }
